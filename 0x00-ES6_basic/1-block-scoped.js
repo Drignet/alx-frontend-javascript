6ad7679ee34c5621d2d502de5ee80edf6e25f5e2
drignet <odibehenry@gmail.com>
2023-03-20 16:11:13 -0700

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
-  let task2 = true;
+  const task2 = true;
 
   if (trueOrFalse) {
     task = true;
-    task2 = false;
+    // task2 cannot be reassigned since it is declared as a constant
   }
 
   return [task, task2];
